2a86939053dd28fb13b1022d1349d2adf83ed68d
Jungbum Woo <jungbumwoo@naver.com>
2023-04-29 16:32:03 +0900

    initial scanner

---
+++ interpreter_first/src/com/craftinginterpreters/lox/Main.java
@@ -0,0 +1,9 @@
+package com.craftinginterpreters.lox;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("main executed");
+    }
+}
