4a4df38b5a0c7615e38cb98c87766bbd418385a0
Jungbum Woo <jungbumwoo@naver.com>
2023-05-07 11:35:51 +0900

    add AstPrinter

--- interpreter_first/src/com/craftinginterpreters/lox/Lox.java
+++ interpreter_first/src/com/craftinginterpreters/lox/Lox.java
@@ -9,7 +9,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Lox {
     static boolean hadError = false;
