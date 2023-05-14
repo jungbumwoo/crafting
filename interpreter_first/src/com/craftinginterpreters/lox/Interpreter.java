77ac8807ae31c1328b4fbb9c2fd51aeb405eed52
Jungbum Woo <jungbumwoo@naver.com>
2023-05-14 11:45:42 +0900

    Evaluating Expressions

---
+++ interpreter_first/src/com/craftinginterpreters/lox/Interpreter.java
@@ -0,0 +1,92 @@
+package com.craftinginterpreters.lox;
+
+class Interpreter implements Expr.Visitor<Object> {
+
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr){
+        return expr.value;
+    }
+
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr){
+        return evaluate(expr.expression);
+    }
+
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        // Post-order traversal.
+        // each node evaluates its children before doing its own work.
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type){
+            case BANG:
+                return !isTruthy(right);
+            case MINUS:
+                return -(double)right;
+        }
+
+        // Unreachable.
+        return null;
+    }
+
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr){
+        // left-to-right order.
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            // arithmetic operators. always produce value.
+            case MINUS:
+                return (double)left - (double)right;
+            case PLUS:
+                if (left instanceof Double && right instanceof Double){
+                    return (double)left + (double)right;
+                }
+                if (left instanceof String && right instanceof String){
+                    return (String)left + (String)right;
+                }
+                // TODO: Error handling.
+                // throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings.");
+                break;
+            case SLASH:
+                return (double)left / (double)right;
+            case STAR:
+                return (double)left * (double)right;
+
+            // comparison operators. always produce Boolean.
+            case GREATER:
+                return (double) left > (double) right;
+            case GREATER_EQUAL:
+                return (double) left >= (double) right;
+            case LESS:
+                return (double) left < (double) right;
+            case LESS_EQUAL:
+                return (double) left <= (double) right;
+
+            // equality operators.
+            case BANG_EQUAL: return !isEqual(left, right);
+            case EQUAL_EQUAL: return isEqual(left, right);
+
+        }
+
+        // Unreachable.
+        return null;
+    }
+
+    private boolean isTruthy(Object object){
+        // What is Truth?
+        // false, null -> false else true.
+        if (object == null) return false;
+        if (object instanceof Boolean) return (boolean)object;
+        return true;
+    }
+
+    private boolean isEqual(Object a, Object b){
+        // nil is only equal to nil.
+        if (a == null && b == null) return true;
+        if (a == null) return false;
+        // if (b == null) return false; <- 얘는 필요 없나?
+        return a.equals(b);
+    }
+}
