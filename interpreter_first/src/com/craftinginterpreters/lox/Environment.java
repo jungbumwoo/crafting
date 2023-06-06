e0cd0a85181623385521ae26dd082d13db34255f
Matt <matthew.woo@buzzvil.com>
2023-06-06 19:48:54 +0900

    assignment 관련 interpreter 반영하기

--- interpreter_first/src/com/craftinginterpreters/lox/Environment.java
+++ interpreter_first/src/com/craftinginterpreters/lox/Environment.java
@@ -16,6 +16,17 @@ public class Environment {
     }
 
     // 변수 명(name) 에 변수 값(value) 설정
+    // get() 과 다른 점은 이미 존재하는 변수에 대해서만 값을 설정한다. 새로 변수를 생성하지 않음.
+    void assign(Token name, Object value) {
+        if (values.containsKey(name.lexeme)) {
+            values.put(name.lexeme, value);
+            return;
+        }
+
+        throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
+    }
+
+    // 변수 명(name) 에 변수 값(value) 설정
     void define(String name, Object value){
         values.put(name, value);
     }
