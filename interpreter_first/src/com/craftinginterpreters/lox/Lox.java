1bfb0ab3e38fe26a4f9c825408eefeb374fc9919
jungbumwoo <jungbumwoo@naver.com>
2023-12-25 00:38:06 +0900

    11.5 Resolution Errors

--- interpreter_first/src/com/craftinginterpreters/lox/Lox.java
+++ interpreter_first/src/com/craftinginterpreters/lox/Lox.java
@@ -73,6 +73,9 @@ public class Lox {
         Resolver resolver = new Resolver(interpreter);
         resolver.resolve(statements);
 
+        // Stop if there was a resolution error.
+        if (hadError) return;
+
         // start execute the syntax tree
         interpreter.interpret(statements);
     }
--- interpreter_first/src/com/craftinginterpreters/lox/Resolver.java
+++ interpreter_first/src/com/craftinginterpreters/lox/Resolver.java
@@ -15,9 +15,15 @@ import java.util.Stack;
 class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
     private final Interpreter interpreter;
     private final Stack<Map<String, Boolean>> scopes = new Stack<>();
+    private FunctionType currentFunction = FunctionType.NONE;
 
     Resolver(Interpreter interpreter){
         this.interpreter = interpreter;
+    }
+
+    private enum FunctionType{
+        NONE,
+        FUNCTION
     }
 
     @Override
@@ -63,7 +69,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
 
         // Unlike variables, we define the name eagerly, before resolving the function's body.
         // This lets a function rescursively refer to itself inside its own body.
-        resolveFunction(stmt);
+        resolveFunction(stmt, FunctionType.FUNCTION);
         return null;
     }
 
@@ -89,6 +95,9 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
 
     @Override
     public Void visitReturnStmt(Stmt.Return stmt){
+        if (currentFunction == FunctionType.NONE){
+            Lox.error(stmt.keyword, "Can't return from top-level code.");
+        }
         if (stmt.value != null) resolve(stmt.value);
         return null;
     }
@@ -172,6 +181,9 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
         if (scopes.isEmpty()) return;
 
         Map<String, Boolean> scope = scopes.peek();
+        if (scope.containsKey(name.lexeme)){
+            Lox.error(name, "Already a variable with this name in this scope.");
+        }
         scope.put(name.lexeme, false); // Mark is as "not ready yet" by binding its name to false in the scope map.
     }
 
@@ -191,7 +203,9 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
         // Not found. Assume it is global.
     }
 
-    private void resolveFunction(Stmt.Function function){
+    private void resolveFunction(Stmt.Function function, FunctionType type){
+        FunctionType enclosingFunction = currentFunction;
+        currentFunction = type;
         beginScope();
         for (Token param : function.params){
             declare(param);
@@ -199,5 +213,6 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
         }
         resolve(function.body);
         endScope();
+        currentFunction = enclosingFunction;
     }
 }
