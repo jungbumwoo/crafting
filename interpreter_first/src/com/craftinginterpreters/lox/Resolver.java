040381f3e908154721d6b3752106ff9093abc819
jungbumwoo <jungbumwoo@naver.com>
2023-12-24 11:39:09 +0900

    11.3.6 Resolving the other syntax tree nodes

--- interpreter_first/src/com/craftinginterpreters/lox/Resolver.java
+++ interpreter_first/src/com/craftinginterpreters/lox/Resolver.java
@@ -67,6 +67,81 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
         return null;
     }
 
+    @Override
+    public Void visitExpressionStmt(Stmt.Expression stmt){
+        resolve(stmt.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitIfStmt(Stmt.If stmt){
+        resolve(stmt.condition);
+        resolve(stmt.thenBranch);
+        if (stmt.elseBranch != null) resolve(stmt.elseBranch);
+        return null;
+    }
+
+    @Override
+    public Void visitPrintStmt(Stmt.Print stmt){
+        resolve(stmt.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitReturnStmt(Stmt.Return stmt){
+        if (stmt.value != null) resolve(stmt.value);
+        return null;
+    }
+
+    @Override
+    public Void visitWhileStmt(Stmt.While stmt){
+        resolve(stmt.condition);
+        resolve(stmt.body);
+        return null;
+    }
+
+    @Override
+    public Void visitBinaryExpr(Expr.Binary expr){
+        resolve(expr.left);
+        resolve(expr.right);
+        return null;
+    }
+
+    @Override
+    public Void visitCallExpr(Expr.Call expr){
+        resolve(expr.callee);
+
+        for (Expr argument : expr.arguments){
+            resolve(argument);
+        }
+
+        return null;
+    }
+
+    @Override
+    public Void visitGroupingExpr(Expr.Grouping expr){
+        resolve(expr.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitLiteralExpr(Expr.Literal expr){
+        return null;
+    }
+
+    @Override
+    public Void visitLogicalExpr(Expr.Logical expr){
+        resolve(expr.left);
+        resolve(expr.right);
+        return null;
+    }
+
+    @Override
+    public Void visitUnaryExpr(Expr.Unary expr){
+        resolve(expr.right);
+        return null;
+    }
+
     void resolve(List<Stmt> statements){
         for (Stmt statement : statements){
             resolve(statement);
