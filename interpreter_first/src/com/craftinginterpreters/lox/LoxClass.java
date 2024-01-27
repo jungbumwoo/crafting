33470a25e48548f3c9447664c1519174d89a7b87
jungbumwoo <jungbumwoo@naver.com>
2024-01-27 12:04:30 +0900

    12.2 Creating Instance

--- interpreter_first/src/com/craftinginterpreters/lox/LoxClass.java
+++ interpreter_first/src/com/craftinginterpreters/lox/LoxClass.java
@@ -3,7 +3,7 @@ package com.craftinginterpreters.lox;
 import java.util.List;
 import java.util.Map;
 
-class LoxClass {
+class LoxClass implements LoxCallable {
     final String name;
 
     LoxClass(String name){
@@ -13,5 +13,16 @@ class LoxClass {
     @Override
     public String toString(){
         return "class: " + name;
+    }
+
+    @Override
+    public Object call(Interpreter interpreter, List<Object> arguments){
+        LoxInstance instance = new LoxInstance(this);
+        return instance;
+    }
+
+    @Override
+    public int arity(){ // arity() mathod is how interpreter validates that you passed the right number of arguments to callable.
+        return 0;
     }
 }
