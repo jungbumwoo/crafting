78eea2470676d3c82cd51876b607f98fc81fbccb
jungbumwoo <jungbumwoo@naver.com>
2024-02-04 16:54:33 +0900

    12.6 Constructors and Initializers

--- interpreter_first/src/com/craftinginterpreters/lox/LoxClass.java
+++ interpreter_first/src/com/craftinginterpreters/lox/LoxClass.java
@@ -20,12 +20,19 @@ class LoxClass implements LoxCallable {
     @Override
     public Object call(Interpreter interpreter, List<Object> arguments){
         LoxInstance instance = new LoxInstance(this);
+        LoxFunction initializer = findMethod("init");
+        if (initializer != null){
+            initializer.bind(instance).call(interpreter, arguments);
+        }
+
         return instance;
     }
 
     @Override
     public int arity(){ // arity() mathod is how interpreter validates that you passed the right number of arguments to callable.
-        return 0;
+        LoxFunction initializer = findMethod("init");
+        if (initializer == null) return 0;
+        return initializer.arity();
     }
 
     LoxFunction findMethod(String name){
