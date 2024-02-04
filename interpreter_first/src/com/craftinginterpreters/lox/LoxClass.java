291fc42b45aee63e001b738e822b7dec0450b2c1
jungbumwoo <jungbumwoo@naver.com>
2024-02-04 23:59:29 +0900

    13.2 Inheriting Methods

--- interpreter_first/src/com/craftinginterpreters/lox/LoxClass.java
+++ interpreter_first/src/com/craftinginterpreters/lox/LoxClass.java
@@ -41,6 +41,10 @@ class LoxClass implements LoxCallable {
         if (methods.containsKey(name)){
             return methods.get(name);
         }
+
+        if (superclass != null){
+            return superclass.findMethod(name);
+        }
         return null;
     }
 }
