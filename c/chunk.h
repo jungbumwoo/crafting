0a1192bbf98f410518205a3ba03df7ffd3c2c841
jungbumwoo <jungbumwoo@naver.com>
2024-03-03 10:45:34 +0900

    14.5.2 Value arrays

--- c/chunk.h
+++ c/chunk.h
@@ -2,6 +2,7 @@
 #define clox_chunk_h
 
 #include "common.h"
+#include "value.h"
 
 typedef enum {
     OP_RETURN,  // this instruction will mean "return from the current func."
@@ -12,10 +13,12 @@ typedef struct {
     int capacity;
 
     uint8_t* code;
+    ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
 void writeChunk(Chunk* chunk, uint8_t byte);
+int addConstant(Chunk* chunk, Value value);
 
 #endif
