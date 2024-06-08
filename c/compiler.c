1390167ee124ec7adb376000ea2cedd66f2babea
jungbumwoo <jungbumwoo@naver.com>
2024-06-08 13:27:16 +0900

    ~19.4. Operations on Strings

--- c/compiler.c
+++ c/compiler.c
@@ -236,6 +236,11 @@ static void number() {
     emitConstant(NUMBER_VAL(value));
 }
 
+static void string() {
+    // [0, length -1] 이 아닌 이유는 leading and trailing quotation mark 를 제거하기 위함.
+    emitConstant(OBJ_VAL(copyString(
+        parser.previous.start + 1, parser.previous.length - 2)));
+}
 
 static void unary() {
     /*
@@ -286,7 +291,7 @@ ParseRule rules[] = {
         [TOKEN_LESS]          = {NULL, binary, PREC_COMPARISON},
         [TOKEN_LESS_EQUAL]    = {NULL, binary, PREC_COMPARISON},
         [TOKEN_IDENTIFIER]    = {NULL, NULL, PREC_NONE},
-        [TOKEN_STRING]        = {NULL, NULL, PREC_NONE},
+        [TOKEN_STRING]        = {string, NULL, PREC_NONE},
         [TOKEN_NUMBER]        = {number, NULL, PREC_NONE},
         [TOKEN_AND]           = {NULL, NULL, PREC_NONE},
         [TOKEN_CLASS]         = {NULL, NULL, PREC_NONE},
