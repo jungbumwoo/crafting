70279bfa23fb3a459f2f602861d912c955e373b0
jungbumwoo <jungbumwoo@naver.com>
2024-09-17 16:35:23 +0900

    20.4.6 Counting tombstones

--- c/table.c
+++ c/table.c
@@ -71,6 +71,7 @@ static void adjustCapacity(Table* table, int capacity) {
     }
 
     // copy the existing entries into the new array.
+    table->count = 0;
     for (int i = 0; i < table->capacity; i++) {
         Entry* entry = &table->entries[i];
         if (entry->key == NULL) continue;
@@ -79,6 +80,7 @@ static void adjustCapacity(Table* table, int capacity) {
         Entry* dest = findEntry(entries, capacity, entry->key);
         dest->key = entry->key;
         dest->value = entry->value;
+        table->count++;
     }
 
     FREE_ARRAY(Entry, table->entries, table->capacity);
@@ -97,6 +99,7 @@ bool tableSet(Table *table, ObjString* key, Value value) {
     Entry* entry = findEntry(table->entries, table->capacity, key);
 
     bool isNewKey = entry->key == NULL;
+    // tombstone 을 재활용하는 case는 count가 증가되지 않는다.
     if (isNewKey) table->count++;
 
     entry->key = key;
