c07d4bb846f67b7fd03d4c44e8ee153f88a18c01
jungbumwoo <jungbumwoo@naver.com>
2024-09-29 10:54:26 +0900

    21.1.2 Expression statements

--- c/compiler.c
+++ c/compiler.c
@@ -235,6 +235,12 @@ static void expression() {
     parsePrecedence(PREC_ASSIGNMENT);
 }
 
+static void expressionStatement() {
+    expression();
+    consume(TOKEN_SEMICOLON, "Expect ';' after expression.");
+    emitByte(OP_POP);
+}
+
 static void printStatement() {
     expression();
     consume(TOKEN_SEMICOLON, "Expect ';' after value.");
@@ -248,6 +254,8 @@ static void declaration() {
 static void statement() {
     if (match(TOKEN_PRINT)) {
         printStatement();
+    } else {
+        expressionStatement();
     }
 }
 
--- c/vm.c
+++ c/vm.c
@@ -115,6 +115,7 @@ static InterpreterResult run() {
             case OP_NIL: push(NIL_VAL); break;
             case OP_TRUE: push(BOOL_VAL(true)); break;
             case OP_FALSE: push(BOOL_VAL(false)); break;
+            case OP_POP: pop(); break;
             case OP_EQUAL: {
                 Value b = pop();
                 Value a = pop();
