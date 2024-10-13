6a934f80fd644eb38182bc17babc821a05094a76
jungbumwoo <jungbumwoo@naver.com>
2024-10-13 15:21:08 +0900

    23.3 While Statements

--- c/compiler.c
+++ c/compiler.c
@@ -141,6 +141,18 @@ static int emitJump(uint8_t instruction) {
     return currentChunk()->count - 2;
 }
 
+static void emitLoop(int loopStart) {
+    emitByte(OP_LOOP);
+
+    int offset = currentChunk()->count - loopStart + 2;
+    if (offset > UINT16_MAX) {
+        error("Loop body too large.");
+    }
+
+    emitByte((offset >> 8) & 0xff);
+    emitByte(offset & 0xff);
+}
+
 static void emitReturn() {
     /*
      * When run clox, it will parse, compile, and execute a single expression, then print the result.
@@ -508,6 +520,30 @@ static void printStatement() {
     emitByte(OP_PRINT);
 }
 
+/*
+    - condition expression
+        (1) OP_JUMP_IF_FALSE -> false 이면 (4) 로 이동
+        (2) OP_POP: 조건 결과 stack에서 제거
+    - body statement
+        (3) OP_LOOP -> condition expression 으로 이동
+        (4) OP_POP
+    - continue..
+*/
+static void whileStatement() {
+    int loopStart = currentChunk()->count;
+    consume(TOKEN_LEFT_PAREN, "Expect '(' after 'while'.");
+    expression();
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after condition.");
+
+    int exitJump = emitJump(OP_JUMP_IF_FALSE);
+    emitByte(OP_POP);
+    statement();
+    emitLoop(loopStart);
+
+    patchJump(exitJump);
+    emitByte(OP_POP);
+}
+
 static void synchronize() {
     parser.panicMode = false;
 
@@ -548,6 +584,8 @@ static void statement() {
         printStatement();
     } else if (match(TOKEN_IF)) {
         ifStatement();
+    } else if (match(TOKEN_WHILE)){
+        whileStatement();
     } else if (match(TOKEN_LEFT_BRACE)) {
         beginScope();
         block();
--- c/vm.c
+++ c/vm.c
@@ -227,6 +227,11 @@ static InterpreterResult run() {
                 if (isFalsey(peek(0))) vm.ip += offset;
                 break;
             }
+            case OP_LOOP: {
+                uint16_t offset = READ_SHORT();
+                vm.ip -= offset;
+                break;
+            }
             case OP_RETURN: {
                 // for real func, have to change this.
                 // but for now, just print the value.
